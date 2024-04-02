d28a165f7835063edd350cba5463e938159d546b
faizmaqsood668@gmail.com <faizmaqsood668@gmail.com>
2024-04-03 03:23:57 +0500

    web app

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const phones = [
+
+// mobile phones started 
+
+var  phones = [
     {
         brand: 'Apple',
         model: 'Iphone 15 pro max',
@@ -7,24 +10,116 @@ const phones = [
         mainCamera: '48 MP + 12 MP + 12 MP',
         frontCamera: '12 MP',
         price: "Rs. 262,990"
-    }
+    },
+
+    {
+        brand: 'Samsung',
+        model: 'S24 ultra',
+        ram: '12 GB',
+        rom: '512 GB',
+        mainCamera: '200 MP + 50 MP + 10 MP',
+        frontCamera: '12 MP',
+        price: "Rs. 474,999"
+    },
+
+    {
+        brand: 'Samsung',
+        model: 'A 14',
+        ram: '4 GB',
+        rom: '128 GB',
+        mainCamera: '50 MP + 2 MP + 2 MP',
+        frontCamera: '13 MP',
+        price: "Rs. 31,950"
+    },
+
+    {
+        brand: 'Oppo',
+        model: 'A 58',
+        ram: '8 GB',
+        rom: '128 GB',
+        mainCamera: '50 MP + 2 MP ',
+        frontCamera: '8 MP',
+        price: "Rs. 48,800"
+    } ,
 ]
 
-const mobile1 = document.querySelector('.mobile-1');
+const brand = document.querySelector('.brand');
+const model = document.querySelector('.model');
+const ram = document.querySelector('.ram');
+const rom = document.querySelector('.rom');
+const mCamera = document.querySelector('.m-camera');
+const fCamera = document.querySelector('.f-camera');
+const price = document.querySelector('.price');
 
-mobile1.innerHTML = `<li>${phones[0].brand}</li>
-<li>${phones[0].model}</li>
-<li>${phones[0].ram}</li>
-<li>${phones[0].rom}</li>
-<li>${phones[0].mainCamera}</li>
-<li>${phones[0].frontCamera}</li>
-<li>${phones[0].price}</li>
+const brand2 = document.querySelector('.brand2');
+const model2 = document.querySelector('.model2');
+const ram2 = document.querySelector('.ram2');
+const rom2 = document.querySelector('.rom2');
+const mCamera2 = document.querySelector('.m-camera2');
+const fCamera2 = document.querySelector('.f-camera2');
+const price2 = document.querySelector('.price2');
+
+
+
+brand.innerHTML = phones[0].brand;
+model.innerHTML = phones[0].model;
+ram.innerHTML = phones[0].ram;
+rom.innerHTML = phones[0].rom;
+mCamera.innerHTML = phones[0].mainCamera;
+fCamera.innerHTML = phones[0].frontCamera;
+price.innerHTML = phones[0].price;
+
+brand2.innerHTML = phones[1].brand;
+model2.innerHTML = phones[1].model;
+ram2.innerHTML = phones[1].ram;
+rom2.innerHTML = phones[1].rom;
+mCamera2.innerHTML = phones[1].mainCamera;
+fCamera2.innerHTML = phones[1].frontCamera;
+price2.innerHTML = phones[1].price;
+
+
+const brand3 = document.querySelector('.brand3');
+const model3 = document.querySelector('.model3');
+const ram3 = document.querySelector('.ram3');
+const rom3 = document.querySelector('.rom3');
+const mCamera3 = document.querySelector('.m-camera3');
+const fCamera3 = document.querySelector('.f-camera3');
+const price3 = document.querySelector('.price3');
+
+const brand4 = document.querySelector('.brand4');
+const model4 = document.querySelector('.model4');
+const ram4 = document.querySelector('.ram4');
+const rom4 = document.querySelector('.rom4');
+const mCamera4 = document.querySelector('.m-camera4');
+const fCamera4 = document.querySelector('.f-camera4');
+const price4 = document.querySelector('.price4');
+
+
+
+brand3.innerHTML = phones[2].brand;
+model3.innerHTML = phones[2].model;
+ram3.innerHTML = phones[2].ram;
+rom3.innerHTML = phones[2].rom;
+mCamera3.innerHTML = phones[2].mainCamera;
+fCamera3.innerHTML = phones[2].frontCamera;
+price3.innerHTML = phones[2].price;
+
+brand4.innerHTML = phones[3].brand;
+model4.innerHTML = phones[3].model;
+ram4.innerHTML = phones[3].ram;
+rom4.innerHTML = phones[3].rom;
+mCamera4.innerHTML = phones[3].mainCamera;
+fCamera4.innerHTML = phones[3].frontCamera;
+price4.innerHTML = phones[3].price;
 
 
 
 
+// mobile phones ended 
 
 
-`
+// add button started 
 
 
+
+// add button ended
