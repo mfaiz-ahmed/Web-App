a49e11cdf4f88ae34c5914b151491e7e0c1ff075
faizmaqsood668@gmail.com <faizmaqsood668@gmail.com>
2024-04-06 00:57:33 +0500

    footer added

--- app.js
+++ app.js
@@ -1,125 +1,73 @@
-
-// mobile phones started 
-
-var  phones = [
-    {
-        brand: 'Apple',
-        model: 'Iphone 15 pro max',
-        ram: '8 GB',
-        rom: '256 GB',
-        mainCamera: '48 MP + 12 MP + 12 MP',
-        frontCamera: '12 MP',
-        price: "Rs. 262,990"
-    },
-
+const phones = [
     {
         brand: 'Samsung',
-        model: 'S24 ultra',
-        ram: '12 GB',
-        rom: '512 GB',
-        mainCamera: '200 MP + 50 MP + 10 MP',
-        frontCamera: '12 MP',
-        price: "Rs. 474,999"
+        model: 'S20',
+        ram: 8,
+        rom: 256,
+        camera: '20 megapixel',
+        price: 15000
     },
-
     {
-        brand: 'Samsung',
-        model: 'A 14',
-        ram: '4 GB',
-        rom: '128 GB',
-        mainCamera: '50 MP + 2 MP + 2 MP',
-        frontCamera: '13 MP',
-        price: "Rs. 31,950"
+        brand: 'Xiomi',
+        model: 'note10',
+        ram: 4,
+        rom: 64,
+        camera: '10 megapixel',
+        price: 15000
     },
-
+    {
+        brand: 'Infinix',
+        model: 'z10',
+        ram: 2,
+        rom: 16,
+        camera: '5 megapixel',
+        price: 15000
+    },
+    {
+        brand: 'Tecno',
+        model: 'spark10',
+        ram: 12,
+        rom: 512,
+        camera: '25 megapixel',
+        price: 15000
+    },
+    {
+        brand: 'Iphone',
+        model: '14',
+        ram: 4,
+        rom: 1024,
+        camera: '30 megapixel',
+        price: 15000
+    },
     {
         brand: 'Oppo',
-        model: 'A 58',
-        ram: '8 GB',
-        rom: '128 GB',
-        mainCamera: '50 MP + 2 MP ',
-        frontCamera: '8 MP',
-        price: "Rs. 48,800"
-    } ,
+        model: 'F11',
+        ram: 8,
+        rom: 256,
+        camera: '20 megapixel',
+        price: 15000
+    },
+    {
+        brand: 'Vivo',
+        model: 'y20',
+        ram: 4,
+        rom: 64,
+        camera: '8 megapixel',
+        price: 15000
+    },
+    {
+        brand: 'Abdullah',
+        model: 's50',
+        ram: 50,
+        rom: 1024,
+        camera: '60 megapixel',
+        price: 300000
+    },
+
 ]
 
-const brand = document.querySelector('.brand');
-const model = document.querySelector('.model');
-const ram = document.querySelector('.ram');
-const rom = document.querySelector('.rom');
-const mCamera = document.querySelector('.m-camera');
-const fCamera = document.querySelector('.f-camera');
-const price = document.querySelector('.price');
+const cardText = document.querySelector('.card-text')
 
-const brand2 = document.querySelector('.brand2');
-const model2 = document.querySelector('.model2');
-const ram2 = document.querySelector('.ram2');
-const rom2 = document.querySelector('.rom2');
-const mCamera2 = document.querySelector('.m-camera2');
-const fCamera2 = document.querySelector('.f-camera2');
-const price2 = document.querySelector('.price2');
-
-
-
-brand.innerHTML = phones[0].brand;
-model.innerHTML = phones[0].model;
-ram.innerHTML = phones[0].ram;
-rom.innerHTML = phones[0].rom;
-mCamera.innerHTML = phones[0].mainCamera;
-fCamera.innerHTML = phones[0].frontCamera;
-price.innerHTML = phones[0].price;
-
-brand2.innerHTML = phones[1].brand;
-model2.innerHTML = phones[1].model;
-ram2.innerHTML = phones[1].ram;
-rom2.innerHTML = phones[1].rom;
-mCamera2.innerHTML = phones[1].mainCamera;
-fCamera2.innerHTML = phones[1].frontCamera;
-price2.innerHTML = phones[1].price;
-
-
-const brand3 = document.querySelector('.brand3');
-const model3 = document.querySelector('.model3');
-const ram3 = document.querySelector('.ram3');
-const rom3 = document.querySelector('.rom3');
-const mCamera3 = document.querySelector('.m-camera3');
-const fCamera3 = document.querySelector('.f-camera3');
-const price3 = document.querySelector('.price3');
-
-const brand4 = document.querySelector('.brand4');
-const model4 = document.querySelector('.model4');
-const ram4 = document.querySelector('.ram4');
-const rom4 = document.querySelector('.rom4');
-const mCamera4 = document.querySelector('.m-camera4');
-const fCamera4 = document.querySelector('.f-camera4');
-const price4 = document.querySelector('.price4');
-
-
-
-brand3.innerHTML = phones[2].brand;
-model3.innerHTML = phones[2].model;
-ram3.innerHTML = phones[2].ram;
-rom3.innerHTML = phones[2].rom;
-mCamera3.innerHTML = phones[2].mainCamera;
-fCamera3.innerHTML = phones[2].frontCamera;
-price3.innerHTML = phones[2].price;
-
-brand4.innerHTML = phones[3].brand;
-model4.innerHTML = phones[3].model;
-ram4.innerHTML = phones[3].ram;
-rom4.innerHTML = phones[3].rom;
-mCamera4.innerHTML = phones[3].mainCamera;
-fCamera4.innerHTML = phones[3].frontCamera;
-price4.innerHTML = phones[3].price;
-
-
-
-
-// mobile phones ended 
-
-
-// add button started 
-
-
-
-// add button ended
+function render(){
+    
+}    
