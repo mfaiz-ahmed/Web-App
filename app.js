21323ef7fd1140a71df831525b453287399cd65f
faizmaqsood668@gmail.com <faizmaqsood668@gmail.com>
2024-04-08 01:58:49 +0500

    web app

--- app.js
+++ app.js
@@ -2,11 +2,122 @@ const phones = [
     {
         image: "./Assets/Mobile-1.webp",
         brand: 'Apple',
-        model: 'Iphone 15 pro max',
-        ram: 8,
-        rom: 256,
-        camera: '20 megapixel',
-        price: 15000
+        model: 'Iphone 15 Pro Max',
+        ram: "8 GB",
+        rom: "256 GB",
+        maincamera: '48 MP + 12 MP + 12 MP',
+        frontCamera: '12 MP',
+        price: "524,999" 
+    },
+    {
+        image: "./Assets/Mobile-2.webp",
+        brand: 'Samsung',
+        model: 'S24 Ultra',
+        ram: "12 GB",
+        rom: "512 GB",
+        maincamera: "200 MP + 50 MP + 10 MP",
+        frontCamera: '12 MP',
+        price: "474,999" 
+    },
+    {
+        image: "./Assets/Mobile-3.webp",
+        brand: 'Samsung',
+        model: 'A15',
+        ram: "3 GB",
+        rom: "32 GB",
+        maincamera: '50 MP + 5 MP + 2 MP',
+        frontCamera: '13 MP',
+        price: "12,499" 
+    },
+    {
+        image: "./Assets/Mobile-4.webp",
+        brand: 'Oppo',
+        model: 'A18',
+        ram: "4 GB",
+        rom: "128 GB",
+        maincamera: "8 MP + 2 MP",
+        frontCamera: '5 MP',
+        price: "474,999" 
+    },
+    {
+        image: "./Assets/Mobile-5.png",
+        brand: 'Oppo',
+        model: 'F1s',
+        ram: "4 GB",
+        rom: "64 GB",
+        maincamera: '13 MP',
+        frontCamera: '16 MP',
+        price: "17,999" 
+    },
+    {
+        image: "./Assets/Mobile-6.png",
+        brand: 'Apple',
+        model: 'Iphone 14',
+        ram: "6 GB",
+        rom: "128 GB",
+        maincamera: "48 MP",
+        frontCamera: '12 MP',
+        price: "168,000" 
+    },
+    {
+        image: "./Assets/Mobile-7.png",
+        brand: 'Samsung',
+        model: 'A04',
+        ram: "3 GB",
+        rom: "32 GB",
+        maincamera: '50 MP',
+        frontCamera: '5 MP',
+        price: "18,780" 
+    },
+    {
+        image: "./Assets/Mobile-8.webp",
+        brand: 'Realme',
+        model: 'C67',
+        ram: "8 GB",
+        rom: "128 GB",
+        maincamera: "108 MP + 2 MP",
+        frontCamera: '8 MP',
+        price: "474,999" 
+    },
+    {
+        image: "./Assets/Mobile-9.webp",
+        brand: 'Realme',
+        model: 'C33',
+        ram: "4 GB",
+        rom: "64 GB",
+        maincamera: '50 MP',
+        frontCamera: '5 MP',
+        price: "29,999" 
+    },
+    {
+        image: "./Assets/Mobile-10.webp",
+        brand: 'Samsung',
+        model: 'Z Flip 4',
+        ram: "8 GB",
+        rom: "512 GB",
+        maincamera: "12 MP + 12 MP",
+        frontCamera: '10 MP',
+        price: "249,999" 
+    },
+    {
+        image: "./Assets/Mobile-11.jpg",
+        brand: 'Oneplus',
+        model: 'Oneplus 10 Pro',
+        ram: "12 GB",
+        rom: "256 GB",
+        maincamera: '48 MP + 50 MP + 8 MP',
+        frontCamera: '32 MP',
+        price: "160,000" 
+    },
+    {
+        image: "./Assets/Mobile-12.webp",
+        brand: 'Tecno',
+        model: 'Spark 20 Pro Plus',
+        ram: "8 GB",
+        rom: "256 GB",
+        maincamera: "108 MP",
+        frontCamera: '32 MP',
+        price: "474,999" 
     },
     
 
@@ -26,12 +137,13 @@ function render(){
         <div class="box">
                     <div class="box-content">
                       <img class="card-img-top" src="${phones.image}" alt="">
-                      <p>Brand:${phones.brand}</p>
-                      <p>Model:${phones.model}</p>
-                      <p>Ram:${phones.ram}</p>
-                      <p>Rom:${phones.rom}</p>
-                      <p>Camera:${phones.camera}</p>
-                      <p>Price:${phones.price}</p>
+                      <p><span class="fw-bold">Brand:</span> ${phones.brand}</p>
+                      <p><span class="fw-bold">Model:</span> ${phones.model}</p>
+                      <p><span class="fw-bold">Ram:</span> ${phones.ram}</p>
+                      <p><span class="fw-bold">Rom:</span> ${phones.rom}</p>
+                      <p><span class="fw-bold">Rear Camera:</span> ${phones.maincamera}</p>
+                      <p><span class="fw-bold">Front Camera:</span> ${phones.frontCamera}</p>
+                      <p><span class="fw-bold">Price:</span> Rs. ${phones.price} </p>
                       
                       <button class="button mt-3">Add To Cart</button>
                     </div>
