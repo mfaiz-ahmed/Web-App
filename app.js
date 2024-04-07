747acb77b78514e0be534bba94397a58f23bfd97
faizmaqsood668@gmail.com <faizmaqsood668@gmail.com>
2024-04-08 00:31:17 +0500

    web app

--- app.js
+++ app.js
@@ -1,68 +1,42 @@
 const phones = [
     {
-        brand: 'Samsung',
-        model: 'S20',
+        image: "./Assets/Mobile-1.webp",
+        brand: 'Apple',
+        model: 'Iphone 15 pro max',
         ram: 8,
         rom: 256,
         camera: '20 megapixel',
         price: 15000
     },
-    {
-        brand: 'Xiomi',
-        model: 'note10',
-        ram: 4,
-        rom: 64,
-        camera: '10 megapixel',
-        price: 15000
-    },
-    {
-        brand: 'Infinix',
-        model: 'z10',
-        ram: 2,
-        rom: 16,
-        camera: '5 megapixel',
-        price: 15000
-    },
-    {
-        brand: 'Tecno',
-        model: 'spark10',
-        ram: 12,
-        rom: 512,
-        camera: '25 megapixel',
-        price: 15000
-    },
-    {
-        brand: 'Iphone',
-        model: '14',
-        ram: 4,
-        rom: 1024,
-        camera: '30 megapixel',
-        price: 15000
-    },
-    {
-        brand: 'Oppo',
-        model: 'F11',
-        ram: 8,
-        rom: 256,
-        camera: '20 megapixel',
-        price: 15000
-    },
-    {
-        brand: 'Vivo',
-        model: 'y20',
-        ram: 4,
-        rom: 64,
-        camera: '8 megapixel',
-        price: 15000
-    },
-    {
-        brand: 'Abdullah',
-        model: 's50',
-        ram: 50,
-        rom: 1024,
-        camera: '60 megapixel',
-        price: 300000
-    },
+    
 
 ]
 
+for (let index = 0; index < phones.length; index++) {
+    
+}
+
+
+const showDetails = document.querySelector('.showDetails')
+
+function render(){
+    showDetails.innerHTML = '';
+    phones.forEach((phones , index)=>{
+        showDetails.innerHTML +=`
+        <div class="box">
+                    <div class="box-content">
+                      <img class="card-img-top" src="${phones.image}" alt="">
+                      <p>Brand:${phones.brand}</p>
+                      <p>Model:${phones.model}</p>
+                      <p>Ram:${phones.ram}</p>
+                      <p>Rom:${phones.rom}</p>
+                      <p>Camera:${phones.camera}</p>
+                      <p>Price:${phones.price}</p>
+                      
+                      <button class="button mt-3">Add To Cart</button>
+                    </div>
+                  </div>` 
+    })
+}
+
+render()
